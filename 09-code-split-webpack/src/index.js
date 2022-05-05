71751a6bbd340e3ce2fc57c81e34e46b400a21bd
王远辉 <3162662415@qq.com>
2022-05-05 21:04:26 +0800

    预加载、与获取

--- 09-code-split-webpack/src/index.js
+++ 09-code-split-webpack/src/index.js
@@ -9,7 +9,17 @@ helloworld()
 const button = document.createElement('button')
 button.textContent = '加法'
 button.addEventListener('click', () => {
-    import(/* webpackChunkName: 'math' */'./math.js').then(({ add }) => {
+    /**
+     * 预获取
+     * 在 script标签上多个 属性
+     */
+    import(/* webpackChunkName: 'math',webpackPrefetch: true */'./math.js').then(({ add }) => {
+        console.log(add(4, 5))
+    })
+    /**
+     * 预加载 ，推荐使用上面个
+     */
+     import(/* webpackChunkName: 'math',webpackPreload: true */'./math.js').then(({ add }) => {
         console.log(add(4, 5))
     })
 })
