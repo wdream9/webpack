bdd38a4bd9af68ce56e1e9714c76cfce3b73927c
王远辉 <3162662415@qq.com>
2022-05-05 20:58:59 +0800

    代码分离，模块懒加载实现

--- 09-code-split-webpack/src/index.js
+++ 09-code-split-webpack/src/index.js
@@ -5,3 +5,12 @@ import './async.js'
 
 // console.log(_.join(["hello","world","loadsh"],"-"))
 helloworld()
+
+const button = document.createElement('button')
+button.textContent = '加法'
+button.addEventListener('click', () => {
+    import(/* webpackChunkName: 'math' */'./math.js').then(({ add }) => {
+        console.log(add(4, 5))
+    })
+})
+document.body.appendChild(button)
