636ef4614afc8d979274dc523c19887c3a740268
王远辉 <3162662415@qq.com>
2022-06-01 22:09:26 +0800

    多页面--htmlwebpackolugin--chunks

--- 21-more-page-part2/webpack.config.js
+++ 21-more-page-part2/webpack.config.js
@@ -9,6 +9,7 @@ module.exports = {
     //     loadsh: 'loadsh'
     // },
     entry: {
+        // 每一项表示一个 chunks
         main: {
             import: [ './src/app.js','./src/app2.js'],
             dependOn: 'loadsh'
@@ -20,6 +21,11 @@ module.exports = {
         loadsh: 'loadsh'
     },
     plugins: [
-        new HtmlWebpackPlugin()
+        new HtmlWebpackPlugin({
+            title:'多页面配置-config',
+            template:'./index.html',
+            inject:'body',// script 放在那个区域
+            chunks: ['main'], // 规定页面加载哪些entry中的chunks
+        })
     ],
 }
