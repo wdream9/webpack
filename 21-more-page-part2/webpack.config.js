7f6ce9034f7bd07d0676dc714cca1d168c497753
王远辉 <3162662415@qq.com>
2022-06-01 22:28:42 +0800

    webpack多页面开发配置完成

--- 21-more-page-part2/webpack.config.js
+++ 21-more-page-part2/webpack.config.js
@@ -12,20 +12,38 @@ module.exports = {
         // 每一项表示一个 chunks
         main: {
             import: [ './src/app.js','./src/app2.js'],
-            dependOn: 'loadsh'
+            dependOn: 'loadsh',
+            filename: 'chanel1/[name].js'
         },
         main3: {
             import:'./src/app3.js',
-            dependOn: 'loadsh'
+            dependOn: 'loadsh',
+            filename: 'chanel2/[name].js'
         },
-        loadsh: 'loadsh'
+        loadsh: {
+            import:['loadsh'],
+            filename: 'common/[name].js'
+        }
     },
     plugins: [
         new HtmlWebpackPlugin({
-            title:'多页面配置-config',
+            title:'多页面-001',
             template:'./index.html',
             inject:'body',// script 放在那个区域
             chunks: ['main'], // 规定页面加载哪些entry中的chunks
+            filename: 'chanel1/index.html',
+            publicPath: 'http://www.aaa.com/',
+        }),
+        new HtmlWebpackPlugin({
+            title:'多页面配置-002',
+            template:'./index2.html',
+            inject:'body',
+            chunks: ['main3','loadsh'],
+            filename: 'chanel2/index2.html',
+            publicPath: 'http://www.bbb.com/',
         })
     ],
+    output: {
+        clean: true
+    }
 }
