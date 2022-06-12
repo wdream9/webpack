267e1f18e828c0ec29959088b3eb9e9c783b6a7b
王远辉 <3162662415@qq.com>
2022-06-12 18:57:07 +0800

    自己开发一个工具模块给别人使用

---
+++ 22-libaray-part2/src/index.js
@@ -0,0 +1,3 @@
+export const add = function(){
+    return 11+11;
+}
---
+++ 22-libaray-part2/webpack.config.js
@@ -0,0 +1,9 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+module.exports = {
+    mode: 'development',
+    entry: './src/index.js',
+    plugins: [
+        new HtmlWebpackPlugin(),
+    ],
+}
