e9f4ff4cdc2676eccc8d4022a675cb3ded760600
王远辉 <3162662415@qq.com>
2022-07-03 11:07:11 +0800

    mergePromise() 按规定输出正解

--- 26-async-await-promise/mergePromise.js
+++ 26-async-await-promise/mergePromise.js
@@ -20,21 +20,43 @@ const ajax3 = () => time(1000).then(() => {
     return 3
 })
 
-function mergePromise() {
+/**
+ * 解法一
+ * @returns Promise
+ */
+//  function mergePromise() {
+//     // 在这里写代码
+//     let data = [];
+//     return new Promise(resolve => {
+//         ajax1().then(res => {
+//             data.push(res)
+//             return ajax2()
+//         }).then(res => {
+//             data.push(res)
+//             return ajax3()
+//         }).then(res => {
+//             data.push(res)
+//             resolve(data)
+//         })
+//     })
+// }
+
+// 解法二
+function mergePromise(ajaxArray) {
     // 在这里写代码
-    let data = [];
-    return new Promise(resolve => {
-        ajax1().then(res => {
-            data.push(res)
-            return ajax2()
-        }).then(res => {
-            data.push(res)
-            return ajax3()
-        }).then(res => {
-            data.push(res)
-            resolve(data)
+    // 存放每个ajax的结果
+    const data = [];
+    let promise = Promise.resolve();
+    ajaxArray.forEach(ajax => {
+        // 第一次的then为了用来调用ajax
+        // 第二次的then是为了获取ajax的结果
+        promise = promise.then(ajax).then(res => {
+            data.push(res);
+            return data; // 把每次的结果返回
         })
     })
+    // 最后得到的promise它的值就是data
+    return promise;
 }
 
 mergePromise([ajax1, ajax2, ajax3]).then(data => {
